1b5f13dfeed1c6c7d88e7d1d7e24a25d70a4bea8
Dietmar Gerald Schrausser <dietmar.schrausser@gmail.com>
2024-02-28 22:03:23 +0100

    Update and rename DOSCOM.C to DOSCOM.c

--- DOSCOM.c
+++ DOSCOM.c
@@ -11,17 +11,17 @@ int main(void)
 //outStream  = fopen("_cpy_.bat", "w" );
 //fprintf	(outStream,"xcopy d:\\EIGENEDATEIEN\\_LAUFENDES\\_C_Programme\\_DOSCOM_\\Release\\DOSCOM.EXE d:\\EIGENEDATEIEN\\_LAUFENDES\\_C_Programme\\_DOSCOM_\\_DOSCOM\\");
 
-system ("cmd"); //-----------------------------------> öffnet eine dosshell im aktuellen verzeichnis  
+system ("cmd"); //-----------------------------------> Ã¶ffnet eine dosshell im aktuellen verzeichnis  
 
 //fclose( outStream );
 
-// kopiert doscom.exe in das mit dem desktop verknüpfte verzeichnis, so kann doscom in ein beliebiges verzeichnis verschoben werden, dort ausgeführt und wieder gelöscht werden ohne am desktop zu fehlen
+// kopiert doscom.exe in das mit dem desktop verknÃ¼pfte verzeichnis, so kann doscom in ein beliebiges verzeichnis verschoben werden, dort ausgefÃ¼hrt und wieder gelÃ¶scht werden ohne am desktop zu fehlen
 system("xcopy d:\\EIGENEDATEIEN\\_LAUFENDES\\_C_Programme\\_DOSCOM_\\Release\\DOSCOM.EXE d:\\EIGENEDATEIEN\\_LAUFENDES\\_C_Programme\\_DOSCOM_\\_DOSCOM\\");
 
 // alternativ via generiertem batchfile
 //system ("_cpy_.bat");
 
-system ("ren DOSCOM.exe nil"); //--------------------> umbenennung in nil zur leichtern löscherkennung
+system ("ren DOSCOM.exe nil"); //--------------------> umbenennung in nil zur leichtern lÃ¶scherkennung
 
 return 0;
 }
